17b0b0120368784ae9baf7e37ca186bb113b1385
mufeng <hanlin_bj@163.com>
2018-09-06 10:37:53 +0800

    Initial commit

---
+++ app/src/main/java/com/mufeng/rxjavademo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mufeng.rxjavademo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.mufeng.rxjavademo.rx.ObservableDemo
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun create(v: View){
+        ObservableDemo.testDo()
+    }
+
+}
---
+++ app/src/main/java/com/mufeng/rxjavademo/rx/ObservableDemo.kt
@@ -0,0 +1,44 @@
+package com.mufeng.rxjavademo.rx
+
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+
+/**
+ * Copyright (c) 山东六牛网络科技有限公司 https://liuniukeji.com
+ *
+ * @Description
+ * @Author (沐枫/hanlin_bj@163.com)
+ * @Copyright Copyright (c) 山东六牛网络科技有限公司 保留所有版权(https://www.liuniukeji.com)
+ * @Date 2018/9/6
+ * @CreateBy android_studio
+ * @Remarks
+ */
+class ObservableDemo {
+
+    companion object {
+        /**
+         * 测试do操作符, 以及Observable的生命周期
+         */
+        fun testDo() {
+            Observable.just("Hello RxJava!")
+                    .doOnNext { Log.e("TAG", "doOnNext: $it") }
+                    .doAfterNext { Log.e("TAG", "doAfterNext: $it") }
+                    .doAfterTerminate { Log.e("TAG", "doAfterTerminate: ") }
+                    .doFinally { Log.e("TAG", "doFinally: ") }
+                    .doOnComplete { Log.e("TAG", "doOnComplete: ") }
+                    .doOnDispose { Log.e("TAG", "doOnDispose: ") }
+                    .doOnEach { Log.e("TAG", "doOnEach: ${if (it.isOnNext) "onNext" else if (it.isOnComplete) "onComplete" else "onError"}") }
+                    .doOnError { Log.e("TAG", "doOnError: ${it.localizedMessage}") }
+                    .doOnLifecycle({Log.e("TAG", "doOnLifecycle: ${it.isDisposed}")},{Log.e("TAG", "doOnLifecycle run: ")})
+                    .doOnSubscribe { Log.e("TAG", "doOnSubscribe: ${it.isDisposed}") }
+                    .doOnTerminate { Log.e("TAG", "doOnTerminate: ") }
+                    .subscribeBy(
+                            onNext = {Log.e("TAG", "onNext: $it")},
+                            onComplete = {Log.e("TAG", "onComplete: ")}
+                    )
+
+        }
+    }
+
+}
